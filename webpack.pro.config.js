69ff6147fdc4e1510f4b1db404eed90d8b1e5d91
jingxuefeng <jingxuefeng@shein.com>
2021-05-10 00:14:26 +0800

    feat:增加 分析bundle结构

--- webpack.pro.config.js
+++ webpack.pro.config.js
@@ -4,17 +4,20 @@ const path = require('path');
 const config = require('./webpack.base.config.js');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
-module.exports = Object.assign({}, config, {
-  mode: 'production',
-  devtool: false,
-  plugins: [
-    ...config.plugins,
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify('production'),
-      },
-    }),
-    new BundleAnalyzerPlugin()
-  ]
-});
+module.exports = (env)=> {
+  const { analysis }  = env
+  const morePlugins=analysis==='true'?new BundleAnalyzerPlugin():[]
+  return Object.assign({}, config, {
+    mode: 'production',
+    devtool: false,
+    plugins: [
+      ...config.plugins,
+      new webpack.DefinePlugin({
+        'process.env': {
+          NODE_ENV: JSON.stringify('production'),
+        },
+      }),
+    ].concat(morePlugins)
+  });
+}
 
